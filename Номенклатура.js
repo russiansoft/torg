2d6512e87efe70babbaad22c8f8f844828ffdf08
russiansoft <russiansoft@mail.ru>
2022-01-31 06:29:08 +0300

    удалено второе изображение куаркода

--- Номенклатура.js
+++ Номенклатура.js
@@ -1,13 +1,39 @@
 
-function Test1()
+function ПечатьЭтикетки()
 {
-	// Поиск элемента sheet-qrcode
-	let nodes = this.parentNode.children;
+	СохранитьКуаркод.call(this);
+	On("link", sheet.id, 'reference="Этикетка" type="print" parent="' + this.Cell.Bind.replace(".", "") + '"');
+}
+
+function FindChildByClass(parent, className)
+{
+	if (!parent)
+		return null;
+	if (!parent.children)
+		return null;
+	let nodes = parent.children;
 	for (let i in nodes)
 	{
 		let node = nodes[i];
-		if (node.className != "sheet-qrcode")
+		if (node.className == className)
+			return node;
+	}
+	return null;
+}
+
+function СохранитьКуаркод()
+{
+	// Поиск элемента sheet-qrcode
+	for (let bind1 in sheet.Binds)
+	{
+		if (!bind1.endsWith(".QRCode"))
 			continue;
+
+		let node = sheet.Binds[bind1];
+		node = FindChildByClass(node, "sheet-flex");
+		node = FindChildByClass(node, "sheet-qrcode");
+		if (!node)
+			break;
 
 		// Поиск на 2 уровне
 		let nodes2 = node;
@@ -27,18 +53,18 @@ function Test1()
 			// Поиск изображения получателя
 			for (let bind in sheet.Binds)
 			{
-				if (!bind.endsWith(".ИзображениеКуаркода"))
+				if (!bind.endsWith(".ИзображениеQRCode"))
 					continue;
 				let cell = sheet.Binds[bind];
-				let name = "qrcode.png";
+				let name = "";
 				let base64 = node2.src;
 				let start = "data:image/png;base64,";
 				if (base64.substr(0, 22) != start)
 					return;
 				base64 = base64.substr(22);
 				let value = "name:" + name + "|" +
-					        "type:png|" +
-					        "data:" + base64 + "|";
+							"type:png|" +
+							"data:" + base64 + "|";
 				cell.SetValue(value, name, null, true);
 				break;
 			}
